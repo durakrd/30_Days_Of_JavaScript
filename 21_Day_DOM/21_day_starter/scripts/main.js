2ba6a8159064147c0c08d20d6bd102d3ee568af7
durakrd <krithik.duraisamy@gmail.com>
2024-10-21 15:54:30 -0700

    help2

---
+++ 21_Day_DOM/21_day_starter/scripts/main.js
@@ -0,0 +1,11 @@
+const titles = document.querySelectorAll('h1')
+titles.forEach((title, i) => {
+  title.style.fontSize = '24px' // all titles will have 24px font size
+  if (i % 2 === 0) {
+    title.style.fontSize = '20px'
+  } else {
+    title.style.fontSize = '30px'
+  }
+})
+
+//test 1 2 3
