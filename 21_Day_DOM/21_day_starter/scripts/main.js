c403650f9dbef62883bc473a458d941e2607e520
durakrd <krithik.duraisamy@gmail.com>
2024-10-22 15:21:01 -0700

    trail git

--- 21_Day_DOM/21_day_starter/scripts/main.js
+++ 21_Day_DOM/21_day_starter/scripts/main.js
@@ -1,11 +1,7 @@
 const titles = document.querySelectorAll('h1')
 titles.forEach((title, i) => {
-  title.style.fontSize = '24px' // all titles will have 24px font size
-  if (i % 2 === 0) {
-    title.style.fontSize = '20px'
-  } else {
-    title.style.fontSize = '30px'
+  title.style.fontSize = '75px' // all titles will have 24px font size
   }
-})
+)
 
 //It's time to get this show on the road
